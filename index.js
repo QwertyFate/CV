2ee27bf34607356f1823e51ca2c9a3464055668e
nafaryusnarga <nafaryusnarga@gmail.com>
2023-03-05 16:45:58 +0800

    added the files

---
+++ index.js
@@ -0,0 +1,8 @@
+$(".nav-item").click(function(event){
+    $(".active" ).removeClass("active");
+    $("#" + event.target.id).addClass("active");
+})
+
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#navprime'
+  })
